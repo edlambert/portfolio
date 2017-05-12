22afbdac6d5d343c56651911788ff96f5cc9bdcc
Ed Lambert <elambert101@gmail.com>
2017-05-12 21:51:40 +0100

    publisher loop rate configurable

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -7,4 +7,9 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/user/git_clones/portfolio/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/user/git_clones/portfolio/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/user/git_clones/portfolio/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/user/git_clones/portfolio/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/user/git_clones/portfolio/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/user/git_clones/portfolio/catkin_ws/src:$ROS_PACKAGE_PATH"
